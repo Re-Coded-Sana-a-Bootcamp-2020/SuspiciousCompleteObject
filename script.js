daa1234ff09ba789ff7d8287433c3de4303b9e81
Osama Gharad <mggg1996400@gmail.com>
2020-07-18 10:30:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//  for(i=1;i<6;i++){
+//    console.log(i);
+
+//  }
+
+ for(i=1;i<=7;i++){
+   
+ switch (i) {
+  
+  case 1:
+     day="monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(day);
+
+ }
+ 
